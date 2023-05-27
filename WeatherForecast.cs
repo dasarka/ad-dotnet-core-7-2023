1b5beb966ce1ed34b0e01726ed20352dbbc00ddf
Arka Das <dasarka1020@outlook.com>
2023-05-27 17:46:12 +0530

    initial commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace ad_dotnet_core_7_2023;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
